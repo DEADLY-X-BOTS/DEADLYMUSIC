7874110abd727a3a2ddf7c9b6e91ba9044001bda
#Lord Lucifer ❤️ <137722535+DEADLY-X-BOTS@users.noreply.github.com>
2023-08-01 12:31:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DEADLY-X-BOTS/DEADLYMUSIC")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "https://github.com/DEADLY-X-BOTS/DEADLYMUSIC")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ehdeewafa")
